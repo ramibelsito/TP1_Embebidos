07b6102450ef3e812ec0431d853f8806e8904f6b
ramibelsito <rabelsito@itba.edu.ar>
2026-04-09 14:19:16 -0300

    agregue una capa de leds y el encoder funciona joya

--- Codigo/source/hal/board.h
+++ Codigo/source/hal/board.h
@@ -10,10 +10,10 @@
 /*******************************************************************************
  * INCLUDE HEADER FILES
  ******************************************************************************/
-
-#include "mcal/gpio.h"
 #include <stdbool.h>
 #include <stdint.h>
+
+#include "gpio.h"
 
 
 /*******************************************************************************
@@ -23,20 +23,23 @@
 /***** BOARD defines **********************************************************/
 
 // On Board User LEDs
-#define PIN_LED_RED     PORTNUM2PIN(PB,22) // PTB22
-#define PIN_LED_GREEN   PORTNUM2PIN(PE,26) // PTE26
+#define PIN_LED_RED     PORTNUM2PIN(PB,22)// ???
+#define PIN_LED_GREEN   PORTNUM2PIN(PE,26)// ???
 #define PIN_LED_BLUE    PORTNUM2PIN(PB,21) // PTB21
 
 #define LED_ACTIVE      LOW
 
 
 // On Board User Switches
-#define PIN_SW2         PORTNUM2PIN(PC,6)	// PTC6
-#define PIN_SW3         PORTNUM2PIN(PA,4)	// PTA4
+#define PIN_SW2         // ???
+#define PIN_SW3         PORTNUM2PIN(PA,4) // PTA4
 
-#define SW_ACTIVE       LOW
-#define SW_INPUT_TYPE   BOOL
+#define SW_ACTIVE       LOW // ???
+#define SW_INPUT_TYPE   BOOL // ???
 
+#define PIN_RCHA		PORTNUM2PIN(PA,2)
+#define PIN_RCHB		PORTNUM2PIN(PC,2)
+#define PIN_RCHD		PORTNUM2PIN(PC,3)
 
 /*******************************************************************************
  ******************************************************************************/
