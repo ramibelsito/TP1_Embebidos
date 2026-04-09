6fccde04fbd643c577e6e892b8b905f588ba4260
Franco Morroni <fmorroni99@gmail.com>
2026-04-08 22:23:37 -0300

    chore(shift): fix errors and refactor to use uin16_t instead of bool[16]

--- Codigo/source/hal/shift.c
+++ Codigo/source/hal/shift.c
@@ -1,44 +1,53 @@
-#include "mcal/gpio.h"
 #include "hal/shift.h"
+#include "mcal/gpio.h"
 #include <stdint.h>
 
+#define PIN_SHIFT PORTNUM2PIN(PB, 23)
+#define PIN_CLK PORTNUM2PIN(PA, 1)    // serial clock
+#define PIN_SETOUT PORTNUM2PIN(PB, 9) // latch clock
 
-#define PIN_SHIFT        PORTNUM2PIN(PB,23)
-#define PIN_CLK		     PORTNUM2PIN(PA,1) //serial clock
-#define PIN_SETOUT       PORTNUM2PIN(PB,9) //latch clock
+#define OUTCANT 16
 
-#define OUTCANT		16
+static uint16_t shiftRegister;
 
-
-
-bool shiftarray[OUTCANT];
-
-bool *shiftout = &(shiftarray[0]);
-
-bool shiftOutUpdate()
-{
-	for(i=OUTCANT;i<=0;i--)
-	{
-		gpioWrite(PIN_SHIFT,shiftarray[i]);
-		gpioWrite(PIN_CLK,1);
-		gpioWrite(PIN_CLK,0);
-
-	}
-	gpioWrite(PIN_SETOUT,1);
-	gpioWrite(PIN_SETOUT,0);
-
+uint16_t shiftRead() {
+  return shiftRegister;
 }
 
-
-bool shiftInit()
-{
-	gpioInit(PIN_SHIFT);
-	gpioMode(PIN_SHIFT, OUTPUT);
-	gpioInit(PIN_CLK);
-	gpioMode(PIN_CLK, OUTPUT);
-	gpioWrite(PIN_CLK,0);
-	gpioInit(PIN_SETOUT);
-	gpioMode(PIN_SETOUT, OUTPUT);
-	gpioWrite(PIN_SETOUT,0);
+void shiftWriteWord(uint16_t value) {
+  shiftRegister = value;
 }
 
+void shiftWriteBit(int index, bool state) {
+  if (state) shiftRegister |= (1 << index);
+  else shiftRegister &= ~(1 << index);
+}
+
+bool shiftOutUpdate() {
+  // TODO: averiguar si se puede hacer más eficiente con SPI.
+  for (int i = 15; i >= 0; i--) {
+    gpioWrite(PIN_SHIFT, (shiftRegister >> i) & 1);
+    gpioWrite(PIN_CLK, 1);
+    gpioWrite(PIN_CLK, 0);
+  }
+
+  gpioWrite(PIN_SETOUT, 1);
+  gpioWrite(PIN_SETOUT, 0);
+
+  // TODO: should handle errors?
+  return true;
+}
+
+bool shiftInit() {
+  gpioInit(PIN_SHIFT);
+  gpioMode(PIN_SHIFT, OUTPUT);
+  gpioInit(PIN_CLK);
+  gpioMode(PIN_CLK, OUTPUT);
+  gpioWrite(PIN_CLK, 0);
+  gpioInit(PIN_SETOUT);
+  gpioMode(PIN_SETOUT, OUTPUT);
+  gpioWrite(PIN_SETOUT, 0);
+
+  // TODO: should handle errors?
+  return true;
+}
--- Codigo/source/hal/shift.h
+++ Codigo/source/hal/shift.h
@@ -1,10 +1,17 @@
 #ifndef _SHIFT_H_
 #define _SHIFT_H_
 
-extern bool *shiftout;
+#include <stdint.h>
+#include <stdbool.h>
 
+uint16_t shiftRead();
+void shiftWriteWord(uint16_t value);
+/**
+ * @param index Bit index to modify
+ * @param state `true` set bit to 1, `false` set bit to 0.
+ */
+void shiftWriteBit(int index, bool state);
 bool shiftOutUpdate();
-
 bool shiftInit();
 
 #endif // _SHIFT_H_
