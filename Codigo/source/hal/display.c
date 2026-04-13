767ec49d8fcc84d6698175d3d33889163841bdd9
Franco Morroni <fmorroni99@gmail.com>
2026-04-12 22:47:07 -0300

    chore: make internal display functions static

--- Codigo/source/hal/display.c
+++ Codigo/source/hal/display.c
@@ -1,9 +1,9 @@
 #include "hal/display.h"
+#include "hal/leds.h"
 #include "hal/shift.h"
 #include "mcal/SysTick.h"
 #include <stdbool.h>
 #include <stdint.h>
-#include "hal/leds.h"
 
 #define MAX_STR_LEN 50
 // Whitespace padding after string finishes cycling.
@@ -12,10 +12,11 @@
 // like "345 ", "45  ", "5  1", "  12", " 123", "1234", etc
 #define STR_PADDING 2
 
+// TODO: Remember to fix masks if we change physical connections.
 // NOTE: Asuming the shift registers 0-7 are for the display segments and 10-11 for display digit.
 #define DISPLAY_MASK 0x03FF // 0b0000'0011'1111'1111
-#define SEG_MASK 0x00FF    // 0b0000'0000'1111'1111
-#define DIGIT_MASK 0x300   // 0b0000'0011'0000'0000
+#define SEG_MASK 0x00FF     // 0b0000'0000'1111'1111
+#define DIGIT_MASK 0x300    // 0b0000'0011'0000'0000
 #define DIGIT_SHIFT 8
 
 #define DISPLAY_UPDATE_RATE 10
@@ -27,22 +28,20 @@ static uint8_t displayStringLen = 0;
 static uint8_t dutyPercentage = 100;
 static bool digitBlink[DIGITS] = {0};
 
-//void init_psir();
-void updateDisplay();
-void updateDisplayRegisters(uint8_t segments, uint8_t digit);
-void updateSlideAndBlink();
+static void updateDisplay();
+static void updateDisplayRegisters(uint8_t segments, uint8_t digit);
+static void updateSlideAndBlink();
 
 void initDisplay() {
   pisr_register(updateDisplay, DISPLAY_UPDATE_RATE);
   pisr_register(updateSlideAndBlink, DISPLAY_SLIDE_AND_BLINK_RATE);
   shiftInit();
-  //init_psir();
 }
 
+// TODO: replace for `increseDutyPercentage` and `decreaseDutyPercentage`.
 uint8_t getDutyPercentage() {
   return dutyPercentage;
 }
-
 void setDutyPercentage(uint8_t percentage) {
   if (0 < percentage && percentage <= 100) dutyPercentage = percentage;
 }
@@ -54,6 +53,8 @@ bool writeSegments(uint8_t segments, uint8_t digit) {
 }
 
 static uint8_t char2segments[] = {
+    [0] = 0b00000000,
+
     ['0'] = 0b00111111, ['1'] = 0b00000110, ['2'] = 0b01011011, ['3'] = 0b01001111, ['4'] = 0b01100110,
     ['5'] = 0b01101101, ['6'] = 0b01111101, ['7'] = 0b00000111, ['8'] = 0b01111111, ['9'] = 0b01101111,
 
@@ -64,7 +65,7 @@ static uint8_t char2segments[] = {
     ['U'] = 0b00111110, ['V'] = 0b00111110, ['W'] = 0b00101010, ['X'] = 0b01110110, ['Y'] = 0b01101110,
     ['Z'] = 0b01011011,
 
-    [' '] = 0b00000000, ['-'] = 0b01000000, ['.'] = 0b10000000,
+    [' '] = 0b00000000, ['-'] = 0b01000000, ['_'] = 0b00001000, ['.'] = 0b10000000,
 };
 
 bool writeCharacter(char character, uint8_t digit, bool blink) {
@@ -98,52 +99,7 @@ void cleanDisplay() {
 
 // Private functions
 
-static const char segments2char[256] = {
-    // Digits (preferred)
-    [0b00111111] = '0',
-    [0b00000110] = '1',
-    [0b01011011] = '2',
-    [0b01001111] = '3',
-    [0b01100110] = '4',
-    [0b01101101] = '5',
-    [0b01111101] = '6',
-    [0b00000111] = '7',
-    [0b01111111] = '8',
-    [0b01101111] = '9',
-
-    // Letters (only where distinct or no digit conflict)
-    [0b01110111] = 'A',
-    [0b01111100] = 'B',
-    [0b00111001] = 'C',
-    [0b01011110] = 'D',
-    [0b01111001] = 'E',
-    [0b01110001] = 'F',
-    [0b00111101] = 'G',
-    [0b01110110] = 'H', // also K, X → pick H
-    // 0b00000110 already taken by '1' (I)
-    [0b00011110] = 'J',
-    [0b00111000] = 'L',
-    [0b00010101] = 'M',
-    [0b00110111] = 'N',
-    // 0b00111111 already taken by '0' (O)
-    [0b01110011] = 'P',
-    [0b01100111] = 'Q',
-    [0b00110001] = 'R',
-    // 0b01101101 already taken by '5' (S)
-    [0b01111000] = 'T',
-    [0b00111110] = 'U', // also V → pick U
-    [0b00101010] = 'W',
-    // 0b01110110 already taken (X)
-    [0b01101110] = 'Y',
-    // 0b01011011 already taken by '2' (Z)
-
-    // Symbols
-    [0b00000000] = ' ',
-    [0b10000000] = '.',
-    [0b01000000] = '-',
-};
-
-void updateDisplay() {
+static void updateDisplay() {
   static uint8_t currentDigit = 0;
   static uint8_t duty[DIGITS] = {0};
 
@@ -157,15 +113,14 @@ void updateDisplay() {
   currentDigit = (currentDigit + 1) % DIGITS; // cicleo por los digitos
 }
 
-void updateDisplayRegisters(uint8_t segments, uint8_t digit) {
-
+static void updateDisplayRegisters(uint8_t segments, uint8_t digit) {
   uint16_t shiftRegister = shiftRead();
   shiftRegister = (shiftRegister & ~DISPLAY_MASK) | (segments & SEG_MASK) | ((digit << DIGIT_SHIFT) & DIGIT_MASK);
   shiftWriteWord(shiftRegister);
   shiftOutUpdate();
 }
 
-void updateSlideAndBlink() {
+static void updateSlideAndBlink() {
   static uint8_t slideIdx = 0;
   static uint8_t blinkBuf[DIGITS] = {0};
 
